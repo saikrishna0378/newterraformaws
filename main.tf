2787cbb65b9aabee89a53569bf79885b0dbf614c
Sae <devi.kalisettyaws@gmail.com>
2022-05-26 17:25:12 +0530

    new commit

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+    access_key = "${var.aws_access_key}"
+    secret_key = "${var.aws_secret_key}"
+    region = "${var.aws_region}"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraformb17state"
+    key    = "devopsb1.tfstate"
+    region = "us-east-1"
+  }
+}
